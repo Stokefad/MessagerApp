a392f1b02162d42fb8043dcb819a17998ee7d75f
Igor-Macbook Pro <igor.naum.2013@yandex.ru>
2018-11-19 22:06:04 +0300

    Architecture started

---
+++ MessangerApp/View/MessageCell/ContactCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactCell.swift
+//  MessangerApp
+//
+//  Created by Igor-Macbook Pro on 19/11/2018.
+//  Copyright © 2018 Igor-Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ContactCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
